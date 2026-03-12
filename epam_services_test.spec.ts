9368c96cafcf1728cf08a9cb12a27edeaec30fd5
Alex-Elitea <vrnbsuir@mail.ru>
2026-03-12 21:49:11 +0500

    Create epam_services_test.spec.ts

---
+++ epam_services_test.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('EPAM - Services -> Client Work', () => {
+  test('should navigate from Services to Explore Our Client Work and verify Client Work is visible', async ({ page }) => {
+    // Test steps are added below.
+  });
+});
